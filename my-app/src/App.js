04956b65674dd1b8ed170637bf1a62004f3f30d2
Alessadro Esposito Brescia <alessandro.esposito.brescia@gmail.com>
2022-11-08 09:27:57 -0300

    folders organized

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,42 @@
 import "./global.css";
-import { Card } from "./Card";
-import { Header } from "./Header";
-import img1 from "./image/icon1.png"
-import img2 from "./image/icon2.png"
-import img3 from "./image/icon3.png"
-import img4 from "./image/icon4.png"
+import { Card } from "./components/Card";
+import { Header } from "./components/Header";
+import img1 from "./image/icon1.png";
+import img2 from "./image/icon2.png";
+import img3 from "./image/icon3.png";
+import img4 from "./image/icon4.png";
 // import logo from "./image/ironhack-logo-xs.png"
 // import menu from "./image/menu-top-xs.png"
-
-
 
 function App() {
   return (
     <>
       <Header />
       <div>
-        <Card title="Declarative" description="React makes it painless to create interactive UIs." src={img1} alt="icon1"/>
-        <Card title="Components" description="Build encapsulated components that anage theis state." src={img2} alt="icon2"/>
-        <Card title="Single-Way" description="A set of immutable values are passed to the component's." src={img3} alt="icon3"/>
-        <Card title="JSX" description="Statically-typed, designed to run on modern browsers." src={img4} alt="icon4"/>
+        <Card
+          title="Declarative"
+          description="React makes it painless to create interactive UIs."
+          src={img1}
+          alt="icon1"
+        />
+        <Card
+          title="Components"
+          description="Build encapsulated components that anage theis state."
+          src={img2}
+          alt="icon2"
+        />
+        <Card
+          title="Single-Way"
+          description="A set of immutable values are passed to the component's."
+          src={img3}
+          alt="icon3"
+        />
+        <Card
+          title="JSX"
+          description="Statically-typed, designed to run on modern browsers."
+          src={img4}
+          alt="icon4"
+        />
       </div>
     </>
   );
